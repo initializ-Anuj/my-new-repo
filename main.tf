1db52fb107d4a43ec64e83bc0babdbf89d7983af
AnujSaraswat123 <anujsaraswat712@gmail.com>
2023-08-16 13:56:46 +0530

    my commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "Demo" {
+  ami           = "ami-053b0d53c279acc90"
+  instance_type = "t2.micro"
+  key_name = "mykey"
+  tags = {
+    Name = "git"
+  }
+}
